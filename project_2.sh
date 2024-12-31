1c444429bb4dff3f9188777f2480924059bafc1b
root <root@LAPTOP-Q05BDA81.localdomain>
2024-12-31 18:38:53 -0500

    add project_2.sh

---
+++ project_2.sh
@@ -0,0 +1,14 @@
+# Check the Health of the web server (Apache) hosted on RHEL
+
+# Log file for monitoring
+LOG_FILE="/var/log/health.log"
+
+# Email for alerts
+EMAIL="cyredwin1@gmail.com"
+
+
+# Fonction to save messages to log file
+log_message() {
+echo   "$(date): $1"| tee -a "$LOG_FILE"
+}
+log_message "Your message"
