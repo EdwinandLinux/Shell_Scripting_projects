3be096ccd2c327f62ba5512b36269263629fc5e4
root <root@LAPTOP-Q05BDA81.localdomain>
2025-02-21 10:36:24 -0500

    change the SERVICE_ID code

--- project_6.sh
+++ project_6.sh
@@ -2,10 +2,10 @@
 
 #! /bin/bash
 # Log file location on RHEL
-#LOG_FILE= "/var/log/cron"
-log_file="Jan 30 09:15:11 localhost CRON[12345]: (root) CMD (/usr/bin/ssh)"
+LOG_FILE= "/var/log/cron"
+#log_file="Jan 30 09:15:11 localhost CRON[12345]: (root) CMD (/usr/bin/ssh)"
 # Check if the log file exists
-if [[ ! -f "$LOG_FILE" ]] ; then
+#if [[ ! -f "$LOG_FILE" ]] ; then
     echo " Log file does not exists."
     exit 1
 fi
@@ -14,7 +14,7 @@ echo -e "Month\tDate\tSever Name\tService_ID\tMessage"
 echo -e "-------------------------------------------------------"
 
 # Parse the log file
-grep CRON "$LOG_FILE" | while read -r log_line; do
+#grep CRON "$LOG_FILE" | while read -r log_line; do
      # Extract the Month
      MONTH=$(echo "$log_file" | awk '{print $1}')
      # Extract the Date
@@ -24,7 +24,7 @@ grep CRON "$LOG_FILE" | while read -r log_line; do
      # Extract Message from 5th field to the end
      MESSAGE=$(echo "$log_file" | cut -d ' ' -f6-)
      # Extract Service
-     SERVICE_ID=$(echo "$log_file" | awk -F '[][]' '{print $2}'| cut -d ' ' -f1)
+     SERVICE_ID=$(echo "$log_file" | awk -F '[][]' '{print $2}')
      # Display the data in tabular format
      echo -e "$MONTH\t$DATE\t$SERVER_NAME\t$SERVICE_ID\t\t$MESSAGE"
      done
