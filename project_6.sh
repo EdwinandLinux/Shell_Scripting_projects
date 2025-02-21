bca416d46eecb4d1858330e8d278dc9a984cfc0d
root <root@LAPTOP-Q05BDA81.localdomain>
2025-02-21 10:31:02 -0500

    fixing the issue about printing the Month

--- project_6.sh
+++ project_6.sh
@@ -2,8 +2,8 @@
 
 #! /bin/bash
 # Log file location on RHEL
-LOG_FILE= "/var/log/cron"
-#log_file="Jan 30 09:15:11 localhost CRON[12345]: (root) CMD (/usr/bin/ssh)"
+#LOG_FILE= "/var/log/cron"
+log_file="Jan 30 09:15:11 localhost CRON[12345]: (root) CMD (/usr/bin/ssh)"
 # Check if the log file exists
 if [[ ! -f "$LOG_FILE" ]] ; then
     echo " Log file does not exists."
@@ -16,7 +16,7 @@ echo -e "-------------------------------------------------------"
 # Parse the log file
 grep CRON "$LOG_FILE" | while read -r log_line; do
      # Extract the Month
-     MONTH=$(echo "$line" | awk '{print $1}')
+     MONTH=$(echo "$log_file" | awk '{print $1}')
      # Extract the Date
      DATE=$(echo "$log_file" | awk '{print $2}')
      # Extract the Server Name
@@ -26,7 +26,7 @@ grep CRON "$LOG_FILE" | while read -r log_line; do
      # Extract Service
      SERVICE_ID=$(echo "$log_file" | awk -F '[][]' '{print $2}'| cut -d ' ' -f1)
      # Display the data in tabular format
-     echo -e "$MONTH\t$DATE\t$SERVER_NAME\t$SERVICE_ID\t$MESSAGE"
+     echo -e "$MONTH\t$DATE\t$SERVER_NAME\t$SERVICE_ID\t\t$MESSAGE"
      done
      # Example of log file : Jan 30 09:15:11 localhost CRON[12345]: (root) CMD (/usr/bin/ssh)
-     # Warning: For some reason the command : MONTH=$(echo "$line" | awk '{print $1}') does not print Jan
+     
